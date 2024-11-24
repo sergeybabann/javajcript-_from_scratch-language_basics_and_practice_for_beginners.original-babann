3d278f5a71da796d3e6ca2c7312e184e1163bd3f
sergeybabann <sergeybabann@yandex.ru>
2024-11-24 22:28:51 +0300

    lesson complete: 04 Основы --> 12 Арифметические операторы

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-let a = 2
-console.log(a)
+const width = 10
+const height = 5
+const space = width * height
+const newWidth = width - 4
+const newWidth2 = width + 4
+const division = newWidth / newWidth2
+const volume = 2 ** 3
 
-a = 'test'
-console.log(a)
+console.log(volume)
+
+const city = 'Москва'
+const street = 'Новослободская'
+console.log(city + ', ' + street + ' ' + 5)
