42073a29c1a7601d61e7fdbcf524041611cb932a
sergeybabann <sergeybabann@yandex.ru>
2024-11-25 15:39:56 +0300

    lesson complete: 04 основы --> 15 Типы данных

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
-// порядок исполнения операторов
+// типы данных
+const age = 18 // number
+const surname = 'Иванов' // string
+const isAdmin = true // boolean
+let data // undefined
+let data2 // null
+const admin = Symbol('Admin') // Symbol
+const big = BigInt(999999999999999) // BigInt
 
-const isSuited = 190 - 10 > 90 - 5
-console.log(isSuited)
+// number
+let a = 5
+let b = 5.6
+console.log(typeof a) // number
 
-// 100 - (12-приоритет) 10 > (10-приоритет) 90 - (12-приоритет) 5
+// string
+a = 'строка'
+console.log(typeof a) // string
 
-const a = 6 + 10 / 2 // 1) /  2) +
-const b = (6 + 10) / 2 // 1) + 2) /
-console.log(a)
-console.log(b)
+// boolean
+const isAdmin2 = false //
+console.log(typeof isAdmin2) // boolean
 
-let b1
-let c1
-c1 = b1 = 100 + 50 // 1) +  2) & 3) операторы присваивания по очереди справа налево
-console.log(c1)
-console.log(b1)
+let user_1 = a > 10 //
+console.log(typeof user_1) // boolean
+
+// undefined
+let c
+console.log(typeof c) // undefined
+c = 5
+console.log(typeof c) // number
+
+// null
+let d = null
+console.log(typeof d) // object
+let d2 = null
+console.log(d == null) // true
