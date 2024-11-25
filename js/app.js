5fb82b3cff3373bf0a83bfb7414c2fefa857caa7
sergeybabann <sergeybabann@yandex.ru>
2024-11-25 16:49:47 +0300

    lesson complete: 04 основы --> 17 Шаблонные строки

--- js/app.js
+++ js/app.js
@@ -1,24 +1,16 @@
-/* 
------ Упражнение - Расчет проекта -----
+const projectName = 'Сайт магазина'
+const price = 2000
+const author = 'Василий Пупкин'
 
-Ваша часовая ставка 80$ и вы готовы работать не
-более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40
-часов работы.
-Сейчас понедельник.
-Вы должны уехать через 11 дней.
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
-*/
+const template = author + ' заказал ' + projectName + ' по цене ' + price + '$'
+console.log(template)
 
-// данные
-const payRateUSD = 80 // ставка оплаты в час
-const projectHours = 40 // рабочие часы в неделю
-const availableHours = (11 - 2) * 5
-/* количество дней отведенных на работу для выполнения заказа, -2 потому что
-2 выходных, и есть всего 5 часов в день */
+const template2 = `${author} заказал ${projectName} по цене ${price}$`
+console.log(template2)
 
-// Результат
-console.log('Смогу ли я работать? ' + (availableHours > projectHours))
-console.log('Стоимость работ: ' + projectHours * payRateUSD + '$')
+const template3 = 'Проект \n' + 'Цена: ' + price + '$'
+console.log(template3)
+
+const template4 = `Проект
+Цена: ${price}$`
+console.log(template4)
