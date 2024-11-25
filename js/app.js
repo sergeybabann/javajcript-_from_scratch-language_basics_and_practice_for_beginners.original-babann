b496d1bca3df8116d59000c599a0568144068c14
sergeybabann <sergeybabann@yandex.ru>
2024-11-25 19:29:59 +0300

    lesson complete: 05 управление потоком --> 20 If else

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
-console.log(Boolean(0)) // false
-console.log(Boolean(1)) // true
-console.log(Boolean('')) // false
-console.log(Boolean(undefined)) // false
-console.log(Boolean(null)) // false
-console.log(Boolean(Number('lorem'))) // false
-console.log(Boolean(NaN)) // false
+const money = 10
+const canBuy = money > 50
 
-let a
-console.log(Boolean(a)) // false
+if (canBuy) {
+  console.log('Может купить наш продукт')
+} else {
+  console.log('Не хватает баланса')
+}
+
+console.log('Итог')
+
+// чаще пишут так
+const money2 = 51
+if (money2 > 50) {
+  console.log('Может купить наш продукт 2')
+} else {
+  console.log('Не хватает баланса 2')
+}
+
+// else if
+const money3 = 100
+if (money3 > 50) {
+  console.log('Может купить наш продукт 3')
+} else if (money > 55) {
+  console.log('Куплен mini продукт 3')
+} else {
+  console.log('Не хватает баланса 3')
+}
+
+console.log('Итог 3')
