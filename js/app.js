53e8f241b5e29ca2ade476b7020a14457daf4074
sergeybabann <sergeybabann@yandex.ru>
2024-11-27 15:16:17 +0300

    lesson complete: 07 функции --> 37 Функции в функциях

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-function canAccessWebsite(age) {
-  if (age < 18) {
-    return 'нет'
-  } else {
-    return 'да'
-  }
+const KG_IN_USD = 7
+const KM_IN_USD = 5
+
+function calculateW(present) {
+  return present * KG_IN_USD
 }
 
-console.log(canAccessWebsite(18))
-
-const canAccessWebsite2 = (age) => (age < 18 ? 'нет' : 'да')
-console.log(canAccessWebsite2(19))
+function calculateKm(distance) {
+  return distance * KM_IN_USD
+}
+function getExchangePrice(present1, present2, distance) {
+  const price1 = present1 * KG_IN_USD
+  const price2 = present2 * KG_IN_USD
+  const distancePrice = distance * KM_IN_USD
+  return price1 + price2 + distancePrice
+}
+console.log(getExchangePrice(1, 2, 10))
