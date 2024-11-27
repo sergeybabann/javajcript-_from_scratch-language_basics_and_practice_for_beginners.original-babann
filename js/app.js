a76af28d43621515d506503da6f1e96f93e9c0c3
sergeybabann <sergeybabann@yandex.ru>
2024-11-27 14:34:59 +0300

    lesson complete: 07 функции --> 35 Параметры по умолчанию

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
-/* переписать функцию в стрлочную  */
 function toPower(num, power) {
-  const res = num ** power
+  const power2 = power ?? 2
+  const res = num ** power2
   return res
 }
-
 console.log(toPower(2, 3))
-
-// стрелочная функция
-const toPowerArrow = (num, power) => num ** power
-
-console.log(toPowerArrow(2, 3))
+console.log(toPower(2))
