40e53d5e7f70799011cc4f319142c7535bc1a125
sergeybabann <sergeybabann@yandex.ru>
2024-11-27 13:58:09 +0300

    lesson complete: 07 функции --> 33 Стрелочные функции

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
+// обычная функция
 function powerOfTwo(num) {
+  console.log(num)
   return num * num
 }
+
 console.log(powerOfTwo(5))
 
-const poft = function (num) {
+// стрелочная функция
+// то что до стрелки - это аргументы, то что после стрелки - это то, что будет возвращать функция
+const poft = (num) => {
+  console.log(num)
   return num * num
 }
 console.log(poft(6))
