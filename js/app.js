21a44fd1d4457aca09af74e6ae6ece5011594b16
sergeybabann <sergeybabann@yandex.ru>
2024-11-28 18:39:51 +0300

    03 пункт урока пройден: 08 массивы --> 42 Slice, splice, concat, reverse: 3) метод reverse

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 const roles = ['user', 'admin', 'manager', 'superuser']
+console.log(roles)
+const res = roles.reverse() // метод reverse реверсирует массив, переворачивает с начала в конец.
+console.log(res)
 
-const res = roles.splice(2) // удаляет все элементы из массива начиная с индекса 2
-console.log(res) // вывод удаленных элементов из массива 2, 3
-console.log(roles) // вывод элементов 0, 1
-
-const res2 = roles.splice(2, 1) // 1-ый аргумент указывает с какого индекса начинть
-// 2-ой аргумент указывает длину, которую нужно взять
-console.log(res2) // выводит удаленные из массива элементы
-console.log(roles) // выводит модифицированный массив
-
-const res3 = roles.splice(-1) // удаляет последний элемент из массива
-console.log(res3) //
-console.log(roles) //
+console.log(roles)
